e59c324d804babafa39f58f47330b2ea703e287d
Greg <gregdelozier@gmail.com>
2015-08-31 21:44:44 -0400

    Initial commit of some files from class 1

---
+++ sql/add_some_sounds.sql
@@ -0,0 +1,9 @@
+insert into sounds(kind,sound) values ("cow","moo");
+insert into sounds(kind,sound) values ("dog","arf");
+insert into sounds(kind,sound) values ("horse","neigh");
+insert into sounds(kind,sound) values ("chicken","cluck");
+insert into sounds(kind,sound) values ("snake","hiss");
+
+insert into sample(name,kind) values("Steven","snake");
+
+
